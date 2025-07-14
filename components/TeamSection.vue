d9b801c06f90ceffa00f78f36ec6de3f7878b020
feroma <federico.romanazzo@gmail.com>
2025-07-14 11:17:02 +0200

    section art#1

--- components/TeamSection.vue
+++ components/TeamSection.vue
@@ -1,6 +1,6 @@
 <template>
   <section id="team" class="team-section">
-    <div class="container">
+    <div class="container bg-yellow">
       <h2 class=" text-center section-title t1b" v-html="data.title"></h2>
 
       <!-- Team Carousel -->
@@ -14,23 +14,35 @@
             :key="person.name+index"
             class="team-slide"
           >
+
+
             <div class="staff-member">
+
+              <p class="staff-description t5">{{ person.description }}</p>
+
               <div class="staff-header">
-                <h3 class="staff-name">{{ person.name }}</h3>
+                <h3 class="staff-name serif text-italic mb-0">{{ person.name }}</h3>
                 <p class="staff-role">{{ person.role }}</p>
               </div>
 
-              <p class="staff-description">{{ person.description }}</p>
+
             </div>
+
+
           </swiper-slide>
         </swiper>
 
         <!-- Navigation Arrows -->
-        <div class="team-carousel-prev carousel-arrow carousel-arrow-prev">
-         <arrow-left/>
+        <div class="team-carousel-prev carousel-arrow carousel-arrow-prev arrow-link">
+          <span class="icon">
+             <arrow-left/>
+          </span>
+
         </div>
-        <div class="team-carousel-next carousel-arrow carousel-arrow-next">
-         <arrow-right/>
+        <div class="team-carousel-next carousel-arrow carousel-arrow-next arrow-link">
+           <span class="icon">
+              <arrow-right/>
+           </span>
         </div>
       </div>
     </div>
