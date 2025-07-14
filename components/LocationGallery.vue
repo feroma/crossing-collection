1c31b991b2cb6c83c760b2326ad2e818f75dee86
feroma <federico.romanazzo@gmail.com>
2025-07-14 15:22:04 +0200

    footer

--- components/LocationGallery.vue
+++ components/LocationGallery.vue
@@ -18,21 +18,36 @@
     </div>
 
     <div class="row" v-else>
-      <div class="col-sm-12">
-        <location-card class="padding-resp" :index="index" :location="location"/>
-      </div>
-      <div class="col-sm-10 offset-sm-2 text-right">
-        <picture-wrapper :image="location.gallery[0]"/>
-      </div>
-      <div class="col-sm-6">
+      <div class="col-12">
+        <div class="row">
+          <div class="col-sm-12">
+            <location-card class="padding-resp" :index="index" :location="location"/>
+          </div>
+          <div class="col-sm-10 offset-sm-2 text-right">
+            <picture-wrapper :image="location.gallery[0]"/>
+          </div>
+        </div>
 
-        <picture-wrapper class="padding-resp"  :image="location.gallery[1]"/>
-        <picture-wrapper :image="location.gallery[3]"/>
+        <div class="row align-items-center">
+
+          <div class="col-sm-6">
+            <picture-wrapper class="padding-resp-2"  :image="location.gallery[1]"/>
+          </div>
+          <div class="col-sm-6">
+            <picture-wrapper   :image="location.gallery[2]"/>
+          </div>
+        </div>
+        <div class="row align-items-center">
+          <div class="col-sm-6">
+            <picture-wrapper  :image="location.gallery[3]"/>
+          </div>
+          <div class="col-sm-6">
+            <picture-wrapper class="padding-resp-2"  :image="location.gallery[4]"/>
+          </div>
+        </div>
       </div>
-      <div class="col-sm-6">
-        <picture-wrapper :image="location.gallery[2]"/>
-        <picture-wrapper  class="padding-resp" :image="location.gallery[4]"/>
-      </div>
+
+
     </div>
 
   </div>
